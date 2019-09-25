fc502b28599483db4dfaec2f5e491411a681ead3
Aj Gill <ajgill@ucdavis.edu>
2019-09-24 18:29:41 -0700

    Update main.cpp
    Complete solution using O(n) time, replaced while loop with a for loop in order to make pointer tracking easier which in turn, makes the algorithm easier to code and understand.

--- MinimumSizeSubarraySum209/main.cpp
+++ MinimumSizeSubarraySum209/main.cpp
@@ -12,42 +12,21 @@
 using namespace std;
 
 int minSubArrayLen(int s, vector<int>& nums) {
-    if (nums.size() == 0){
-        return 0;
-    }
-    int beginPtr = 0, endPtr = 0;
-    int arraySize = nums.size();
-    int sum = 0;
-    while(beginPtr < nums.size()-1){
-        //If that sum is smaller, make window larger
-        if(sum < s && endPtr < nums.size()-1){
-            cout << "First conditional\n";
-            sum += nums[endPtr];
-            //To prevent endPtr from adding over and over while at the end
-//            if(endPtr < nums.size()-1 && endCounter < 5){
-//                sum += nums[endPtr];
-//                endCounter++;
-//            }
-            if(endPtr < nums.size()-1){
-                endPtr++;
-            }
-            cout << "beginPtr:" << beginPtr << " beginPtrValue:" << nums[beginPtr] << " endPtr:"<< endPtr << " endPtrValue:" << nums[endPtr] << "\n";
-            cout << "sum after addition: " << sum << "\n";
-            cout << "arraySize: " << arraySize << "\n";
-        }
-        //If sum is larger, compare window size and make window smaller
-        else if(sum >= s){
-            cout << "Second conditional\n";
+    //Arbitrary large starting number for arraySize
+    int beginPtr = 0, arraySize = 100000000, sum = 0;
+    //Replaced while loop with a for loop to keep track of the end pointer for me
+    for(int endPtr = 0; endPtr<nums.size(); endPtr++){
+        sum += nums[endPtr];
+        //If the sum meets the condition, check if the array size is smaller and make the window smaller
+        while(sum >= s) {
+            arraySize = min(endPtr - beginPtr + 1, arraySize);
             sum -= nums[beginPtr];
             beginPtr++;
-            arraySize = min(endPtr - beginPtr, arraySize);
-            cout << "beginPtr:" << beginPtr << " beginPtrValue:" << nums[beginPtr] << " endPtr:"<< endPtr << " endPtrValue:" << nums[endPtr] << "\n";
-            cout << "sum after subtraction: " << sum << "\n";
-            cout << "arraySize: " << arraySize << "\n";
         }
-        else{
-            return arraySize;
-        }
+    }
+    //If no match was found and the arraySize is still the same, return 0
+    if(arraySize == 100000000){
+        return 0;
     }
     return arraySize;
 }
